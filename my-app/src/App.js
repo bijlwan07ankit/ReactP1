209329d0f8c32de2c2db7b1bfd9198ddfe14a1d1
Ankit Bijlwan <136173190+bijlwan07ankit@users.noreply.github.com>
2024-01-30 21:16:16 +0000

    Adding react app and bootstrap

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+let name="Ankit";
+function App() {
+  return (
+    <>
+    <h1>Hello {name}</h1>
+   <div className="abc">Love</div>
+   <div className="container"></div>
+   <nav>
+    <li>Home</li>
+    <li>Contact</li>
+    <li>Explore</li>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laboriosam veniam consectetur id voluptas numquam consequatur quibusdam, earum totam perferendis omnis unde officiis temporibus hic in corporis eaque quod voluptatem!
+    Commodi consequatur quidem, beatae porro omnis labore velit, reiciendis ut dicta doloremque odio impedit voluptatem quas necessitatibus ullam voluptatum deserunt modi nisi ipsa ad reprehenderit tempore. Quidem vel inventore asperiores!
+    Ullam ex officia ea illum voluptatibus maxime in commodi, facere doloribus sint nemo harum, odit ipsum obcaecati. Aliquid voluptas iure suscipit quod quibusdam et porro neque iste! Laboriosam, illum quae!
+    Aperiam vero tempore, voluptatibus architecto, iste laboriosam sint illo earum similique doloribus aliquam, quaerat voluptate iure sapiente. Alias sint, ullam eligendi similique voluptatibus iusto aspernatur enim excepturi rem accusantium distinctio.
+    Accusamus eligendi error in, quia neque recusandae culpa quasi consequatur eum commodi assumenda officia tempore molestiae soluta fugiat cumque totam illum? Et, saepe odit debitis laborum dignissimos esse praesentium possimus.
+   </nav>
+   </>
+  );
+}
+
+export default App;
