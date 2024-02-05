2f97e8f1370d65e66e44b5c44113cfbe5a449bf2
Ankit Bijlwan <136173190+bijlwan07ankit@users.noreply.github.com>
2024-02-05 09:28:18 +0000

    adding accordian

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './component/Navbar.mjs';
-import TextForm from './component/TextForm.mjs';
+// import TextForm from './component/TextForm.mjs';
+import About from './component/About.js';
+
 function App() {
   return (
     <>
 <Navbar title="Textutil2" aboutText="About me"/>
-<div className="container" my-3> 
-<TextForm heading="Enter the text to Analyze below"/>
+<div className="container my-3"> 
+{/* <TextForm heading="Enter the text to Analyze below"/> */}
+<About/>
 </div>
    </>
   );
--- my-app/src/component/TextForm.mjs
+++ my-app/src/component/TextForm.mjs
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-export default function textForm(props) {
+export default function TextForm(props) {
   const handleUpClick=()=>{
     // console.log("Uppercase was clicked");
     setText("You have clicked on handled click"+text);
@@ -7,20 +7,38 @@ export default function textForm(props) {
     setText(newtext);
   }
 
+  const handleloClick=()=>{
+    let lowercase=text.toLowerCase();
+    setText(lowercase);
+  }
   const handleOnChange=(event)=>{
     // console.log("Handle Change");
     setText(event.target.value)
   }
-  const [text, setText] = useState('Enter your Text');
+  const handleclear=()=>{
+    let newText=("");
+    setText(newText);
+  }
+  const [text, setText] = useState('');
   // text="newText"; //wrong way to change the state 
   //  setText("new text");   //correct way to  change the state 
   return (
-    <div>
-<h1>{props.heading}  </h1>
+    <div class="container">
+<h1>{props.heading} </h1>
 <div className="mb-3">
 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-2" onClick={handleloClick}>Convert to Lowercase</button>
+<button className="btn btn-primary mx-2" onClick={handleclear}>Clear</button>
+
+<div className="container my-3">
+  <h2>Your Text Summary</h2>
+  <p>{text.split(" ").length} words , and {text.length}charector</p>
+  <p>{0.008 * text.split(" ").length} minutes Read</p>
+<h2>preview</h2>
+<p>{text}</p>
+</div>
 </div>
   )
 }
